6e83cf70063215153dfacf340a92b51878c2dc34
XxGaboXxVv <128988968+XxGaboXxVv@users.noreply.github.com>
2024-08-21 17:48:26 -0600

    Update BackupController.php

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -83,70 +83,66 @@ public function listBackups()
 }
     
     public function downloadBackup($file_name)
-    {
-        $disk = Storage::disk(config('backup.backup.destination.disks')[0]);
-        $file = config('backup.backup.name') . '/' . $file_name;
+{
+    $backupPath = storage_path('app/laravel-backups/Acacias/') . $file_name;
 
-        if ($disk->exists($file)) {
-            return response()->download($disk->path($file));
+    if (file_exists($backupPath)) {
+        return response()->download($backupPath);
+    } else {
+        return redirect()->back()->with('error', 'El archivo no existe.');
+    }
+}
+
+public function deleteBackup($file_name)
+{
+    try {
+        $backupPath = storage_path('app/laravel-backups/Acacias/') . $file_name;
+
+        if (file_exists($backupPath)) {
+            unlink($backupPath);
+            return redirect()->back()->with('success', 'Respaldo eliminado exitosamente');
         } else {
             return redirect()->back()->with('error', 'El archivo no existe.');
         }
+    } catch (\Exception $e) {
+        Log::error('Error al eliminar el respaldo: ' . $e->getMessage());
+        return redirect()->back()->withErrors(['error' => 'Error al eliminar el respaldo']);
     }
+}
 
-    public function deleteBackup($file_name)
-    {
-        try {
-            $disk = Storage::disk(config('backup.backup.destination.disks')[0]);
-            $file = config('backup.backup.name') . '/' . $file_name;
+public function convertToZip($file_name)
+{
+    try {
+        $backupPath = storage_path('app/laravel-backups/Acacias/') . $file_name;
 
-            if ($disk->exists($file)) {
-                $disk->delete($file);
-                return redirect()->back()->with('success', 'Respaldo eliminado exitosamente');
-            } else {
-                return redirect()->back()->with('error', 'El archivo no existe.');
-            }
-        } catch (\Exception $e) {
-            Log::error('Error al eliminar el respaldo: ' . $e->getMessage());
-            return redirect()->back()->withErrors(['error' => 'Error al eliminar el respaldo']);
+        if (!file_exists($backupPath)) {
+            return redirect()->back()->with('error', 'El archivo no existe.');
         }
-    }
 
-    public function convertToZip($file_name)
-    {
-        try {
-            $disk = Storage::disk(config('backup.backup.destination.disks')[0]);
-            $backupPath = config('backup.backup.name') . '/' . $file_name;
-    
-            if (!$disk->exists($backupPath)) {
-                return redirect()->back()->with('error', 'El archivo no existe.');
-            }
-    
-            // Verifica si el archivo ya está en formato ZIP
-            if (substr($backupPath, -4) === '.zip') {
-                return redirect()->back()->with('error', 'El archivo ya está en formato ZIP.');
-            }
-    
-            $zipFile = str_replace('.sql', '.zip', $backupPath);
-    
-            // Crear un archivo ZIP
-            $zip = new \ZipArchive();
-            if ($zip->open($disk->path($zipFile), \ZipArchive::CREATE) === TRUE) {
-                $zip->addFile($disk->path($backupPath), basename($backupPath));
-                $zip->close();
-    
-                // Eliminar el archivo SQL después de comprimirlo
-                $disk->delete($backupPath);
-    
-                return redirect()->route('backup')->with('success', 'El archivo se ha convertido a ZIP exitosamente.');
-            } else {
-                throw new \Exception('No se pudo crear el archivo ZIP.');
-            }
-        } catch (\Exception $e) {
-            Log::error('Error al convertir el archivo a ZIP: ' . $e->getMessage());
-            return redirect()->back()->withErrors(['error' => 'Error al convertir el archivo a ZIP']);
+        // Verifica si el archivo ya está en formato ZIP
+        if (substr($backupPath, -4) === '.zip') {
+            return redirect()->back()->with('error', 'El archivo ya está en formato ZIP.');
         }
-    }
-    
 
+        $zipFile = str_replace('.sql', '.zip', $backupPath);
+
+        // Crear un archivo ZIP
+        $zip = new \ZipArchive();
+        if ($zip->open($zipFile, \ZipArchive::CREATE) === TRUE) {
+            $zip->addFile($backupPath, basename($backupPath));
+            $zip->close();
+
+            // Eliminar el archivo SQL después de comprimirlo
+            unlink($backupPath);
+
+            return redirect()->route('backup')->with('success', 'El archivo se ha convertido a ZIP exitosamente.');
+        } else {
+            throw new \Exception('No se pudo crear el archivo ZIP.');
+        }
+    } catch (\Exception $e) {
+        Log::error('Error al convertir el archivo a ZIP: ' . $e->getMessage());
+        return redirect()->back()->withErrors(['error' => 'Error al convertir el archivo a ZIP']);
+    }
+}
+    
 }
